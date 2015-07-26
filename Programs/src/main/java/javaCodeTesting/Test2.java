beb8279675267a7b42adff109c0ea888215d274a
Atul Rai <Atul Rai@192.168.8.100>
2015-07-26 07:38:23 +0530

    inner static class test demo

---
+++ Programs/src/main/java/javaCodeTesting/Test2.java
@@ -0,0 +1,17 @@
+package javaCodeTesting;
+
+public class Test2 {
+	
+	 static int x = 9;
+	static class InnerTest {
+		void msg() {
+			System.out.println("Value of variable is: " + x);
+		}
+		
+		public static void main(String[] args) {
+			
+			Test2.InnerTest ob = new Test2.InnerTest();
+			ob.msg();
+		}
+	}
+}
