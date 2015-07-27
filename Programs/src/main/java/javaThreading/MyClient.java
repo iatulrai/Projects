42f2ac137fb22cbafa032601486b3d3c53b6bfc4
Atul Rai <Atul Rai@192.168.8.100>
2015-07-27 09:41:47 +0530

    simple networking later add threads

---
+++ Programs/src/main/java/javaThreading/MyClient.java
@@ -0,0 +1,38 @@
+package javaThreading;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class MyClient {
+	
+	public void chat() {
+		
+		try {
+			
+			Socket s = new Socket("127.0.0.1", 9000);
+			
+			InputStreamReader streamReader = new InputStreamReader(s.getInputStream());
+			
+			BufferedReader reader = new BufferedReader(streamReader);
+			
+			String advice = reader.readLine();
+			
+			System.out.println("Today's Work: " + advice);
+			
+			reader.close();
+			
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		MyClient client = new MyClient();
+		client.chat();
+	}
+
+}
