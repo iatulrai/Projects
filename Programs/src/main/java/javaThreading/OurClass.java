ae2ee21298979d41d2f1f00aa4ee0989fde3ff7c
Atul Rai <Atul Rai@192.168.8.100>
2015-07-28 16:56:21 +0530

    add i to print the index

--- Programs/src/main/java/javaThreading/OurClass.java
+++ Programs/src/main/java/javaThreading/OurClass.java
@@ -4,7 +4,7 @@ public class OurClass {
 	
 	public void run() {
 		for (int i = 0; i < 100; i++ ) {
-			System.out.println(" Hello Java Thread");
+			System.out.println(i + " Hello Java Thread");
 		}
 	}
 	public static void main(String[] args) {
