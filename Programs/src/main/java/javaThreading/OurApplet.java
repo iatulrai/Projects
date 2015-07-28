38c7492dbd682aeac096c5b173c5ce3a112eab5a
Atul Rai <Atul Rai@192.168.8.100>
2015-07-28 17:52:18 +0530

    add serialversionUID

--- Programs/src/main/java/javaThreading/OurApplet.java
+++ Programs/src/main/java/javaThreading/OurApplet.java
@@ -4,6 +4,8 @@ import java.applet.Applet;
 
 public class OurApplet extends Applet {
 	
+	private static final long serialVersionUID = 1L;
+
 	public void init() {
 		
 		OurClass oc = new OurClass();
