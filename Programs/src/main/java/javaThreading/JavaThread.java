239514366eee5dae7f742977b04123f3c9ac3179
Atul Rai <Atul Rai@192.168.8.102>
2015-08-01 13:55:45 +0530

    thread priority cheching

--- Programs/src/main/java/javaThreading/JavaThread.java
+++ Programs/src/main/java/javaThreading/JavaThread.java
@@ -4,18 +4,20 @@ package javaThreading;
 	
 	public void run() {
 		
-		System.out.println("Java thread");
+		System.out.println("new thread");
 		
 	}
 	
 }
-	class JavaThread {
+	
+ class JavaThread {
 	
 	public static void main(String[] args) {
 		
 		Runnable run = new JavaThreadDemo();
 		Thread td = new Thread(run);
 		td.start();
+		System.out.println("main thread");
 		
 	}
 
