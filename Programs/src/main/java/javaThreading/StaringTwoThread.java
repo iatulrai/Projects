5ecbd13e4bbcc1b6fbfb9ed60625820969c1fe9a
Atul Rai <Atul Rai@192.168.8.102>
2015-08-01 18:05:49 +0530

    method added

--- Programs/src/main/java/javaThreading/StaringTwoThread.java
+++ Programs/src/main/java/javaThreading/StaringTwoThread.java
@@ -3,5 +3,28 @@
 package javaThreading;
 
 public class StaringTwoThread implements Runnable{
+	public static void main(String[] args) {
+		
+		StaringTwoThread twoThread = new StaringTwoThread();
+		
+		Thread first = new Thread(twoThread);
+		Thread second = new Thread(twoThread);
+		
+		first.setName("first Thread");
+		second.setName("Second Thread");
+		
+		first.start();
+		second.start();
+	}
+
+	@Override
+	public void run() {
+		
+		for (int i = 0; i < 15; i++) {
+			String name = Thread.currentThread().getName();
+			System.out.println(name + " is running");
+		}
+		
+	}
 
 }
