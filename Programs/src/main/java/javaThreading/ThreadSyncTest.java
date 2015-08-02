aa62bb9872bdad6e393f08b57cfd72c11f56c415
Atul Rai <Atul Rai@192.168.8.102>
2015-08-02 16:29:44 +0530

    add formating style

--- Programs/src/main/java/javaThreading/ThreadSyncTest.java
+++ Programs/src/main/java/javaThreading/ThreadSyncTest.java
@@ -1,35 +1,37 @@
 package javaThreading;
 
 class SyncTest implements Runnable {
-	
+
 	private int balance;
 
 	@Override
-	public synchronized void run() {
+	public synchronized void run() { // add synchronized method
 		for (int i = 0; i < 35; i++) {
 			increment();
 			System.out.println("Balance is " + balance);
 		}
-		
+
 	}
-	
-	public  void increment() {
-		
+
+	public void increment() {
+
 		int i = balance;
 		balance++;
 	}
-	
+
 }
+
 public class ThreadSyncTest {
+
 	public static void main(String[] args) {
-		
+
 		SyncTest tst = new SyncTest();
-		
+
 		Thread a = new Thread(tst);
 		Thread b = new Thread(tst);
-		
+
 		a.start();
 		b.start();
 	}
-	
+
 }
