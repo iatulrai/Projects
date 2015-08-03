53192f909335aef4c04662bbec5c31196ec1c8ab
Atul Rai <Atul Rai@192.168.8.103>
2015-08-03 08:01:22 +0530

    check style formating changes

--- Programs/src/main/java/javaThreading/JavaThread.java
+++ Programs/src/main/java/javaThreading/JavaThread.java
@@ -2,36 +2,35 @@
 
 package javaThreading;
 
-class JavaThreadDemo implements Runnable{
-	
-	public void run() {		// Always use public void run() method to call a thread
-		
+class JavaThreadDemo implements Runnable {
+
+	public void run() { // Always use public void run() method to call a thread
+
 		try {
-			
-			
+
 			for (int i = 0; i < 10; i++) {
-				
-				Thread.sleep(2000);   // add sleep() method to get a chance to other thread to be run
-				
+
+				Thread.sleep(2000); // add sleep() method to get a chance to
+									// other thread to be run
+
 				System.out.println("new thread");
 			}
-		} catch(InterruptedException ex){
+		} catch (InterruptedException ex) {
 			ex.printStackTrace();
 		}
-		
-		
-	}	
+
+	}
 }
-	
+
 class JavaThread {
-	
+
 	public static void main(String[] args) {
-		
+
 		Runnable run = new JavaThreadDemo();
 		Thread td = new Thread(run);
 		td.start();
 		System.out.println("main thread");
-		
+
 	}
 
 }
--- Programs/src/main/java/javaThreading/ThreadExtends.java
+++ Programs/src/main/java/javaThreading/ThreadExtends.java
@@ -3,16 +3,16 @@
 package javaThreading;
 
 public class ThreadExtends extends Thread {
-	
+
 	public void run() {
 		System.out.println("Java Thread-Concretepage.com");
 	}
+
 	public static void main(String[] args) {
-		
+
 		ThreadExtends te = new ThreadExtends();
 		te.start();
-		
+
 		System.out.println("main thread");
 	}
-
 }
