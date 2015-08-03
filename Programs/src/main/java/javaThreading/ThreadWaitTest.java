8b61900fc26f50fd86b8167d87f0776af12be15c
Atul Rai <Atul Rai@192.168.8.103>
2015-08-03 08:12:14 +0530

    check in

--- Programs/src/main/java/javaThreading/ThreadWaitTest.java
+++ Programs/src/main/java/javaThreading/ThreadWaitTest.java
@@ -1,6 +1,24 @@
 package javaThreading;
 
-class ThreadWait implements
 public class ThreadWaitTest {
+
+	public static void main(String[] args) {
+
+		ThreadWait tw = new ThreadWait();
+		tw.start();
+		System.out.println("Total is: " + tw.total);
+	}
+
+}
+
+class ThreadWait extends Thread {
+	int total;
+
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			total += i;
+		}
+
+	}
 
 }
