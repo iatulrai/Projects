cbbe8cb1b0995b9dc7e902c80f29e0ea936bb899
Atul Rai <Atul Rai@AtulRai-PC>
2015-08-24 20:59:40 +0530

    thread pooling

--- Programs/src/main/java/javaThreading/ThreadPool.java
+++ Programs/src/main/java/javaThreading/ThreadPool.java
@@ -1,51 +1,24 @@
-// Java thread pooling example
+// Java thread pooling example part 1
 
 package javaThreading;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-class WorkerThread implements Runnable {
-
-	private String msg;
-
-	public WorkerThread(String s) {
-
-		this.msg = s;
-	}
-
-	public void run() {
-		System.out.println(Thread.currentThread().getName()
-				+ "Start message = " + msg);
-		processmsg();
-		System.out.println(Thread.currentThread().getName() + "END");
-	}
-
-	private void processmsg() {
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-}
-
 public class ThreadPool {
 	public static void main(String[] args) {
-		ExecutorService exe = Executors.newFixedThreadPool(5); // creating a
-																// pool of 5
-																// threads
-
+		ExecutorService executor = Executors.newFixedThreadPool(5);// creating a
+																	// pool of 5
+																	// threads
 		for (int i = 0; i < 10; i++) {
 			Runnable worker = new WorkerThread("" + i);
-			exe.execute(worker); // calling execute method of ExecutorService
+			executor.execute(worker);// calling execute method of
+										// ExecutorService
 		}
-		exe.shutdown();
-		while (!exe.isTerminated()) {
-			System.out.println("Finished all threads");
+		executor.shutdown();
+		while (!executor.isTerminated()) {
 		}
-	}
 
+		System.out.println("Finished all threads");
+	}
 }
