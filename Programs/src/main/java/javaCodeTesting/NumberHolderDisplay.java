e9d4e3da4b6bd9ba67f8bd625c39abe591d9a916
Atul Rai <Atul Rai@AtulRai-PC>
2015-09-04 17:17:10 +0530

    Q&A

---
+++ Programs/src/main/java/javaCodeTesting/NumberHolderDisplay.java
@@ -0,0 +1,16 @@
+package javaCodeTesting;
+
+class NumberHolder {
+	public int anInt;
+	public float aFloat;
+}
+
+public class NumberHolderDisplay {
+	public static void main(String[] args) {
+		NumberHolder aNumberHolder = new NumberHolder();
+		aNumberHolder.anInt = 1;
+		aNumberHolder.aFloat = 2.3f;
+		System.out.println(aNumberHolder.anInt);
+		System.out.println(aNumberHolder.aFloat);
+	}
+}
