f1bc4af4a2c01cdb4c9114ebc398cdff68bfb6c4
Atul Rai <Atul Rai@AtulRai-PC>
2015-09-04 16:01:20 +0530

    reverse of string using StringBuffer class

--- Programs/src/main/java/javaCodeTesting/ReverseString1.java
+++ Programs/src/main/java/javaCodeTesting/ReverseString1.java
@@ -4,7 +4,7 @@ package javaCodeTesting;
 
 public class ReverseString1 {
 	public static void main(String[] args) {
-		
+
 		StringBuffer sb = new StringBuffer("My name is Atul Rai");
 		System.out.println(sb.reverse());
 	}
