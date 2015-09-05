96c2f31ee618bf8283c016493f333e0d0c082911
Atul Rai <Atul Rai@AtulRai-PC>
2015-09-05 15:47:46 +0530

    nornaml class and static class test

---
+++ Programs/src/main/java/javaCodeTesting/StaticClassTest.java
@@ -0,0 +1,16 @@
+package javaCodeTesting;
+
+public class StaticClassTest {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Main");
+		
+		
+	}
+	
+	static class A{
+		System.out.println("Hello Static" );
+	}
+	
+
+}
