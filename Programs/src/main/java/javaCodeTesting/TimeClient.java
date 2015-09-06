d2469404e1a60315748cbdb37be859f64821a593
Atul Rai <Atul Rai@AtulRai-PC>
2015-09-06 09:30:45 +0530

    interface

---
+++ Programs/src/main/java/javaCodeTesting/TimeClient.java
@@ -0,0 +1,16 @@
+package javaCodeTesting;
+
+import java.time.LocalDateTime;
+
+public interface TimeClient {
+
+	void setTime(int hour, int minute, int second);
+
+	void setDate(int day, int month, int year);
+
+	void setDateAndTime(int day, int month, int year, int hour, int minute,
+			int second);
+	
+	LocalDateTime getLocalDateTime();
+
+}
