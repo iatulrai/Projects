e44f499714a0004e87a39595ee83161228923fe8
Atul Rai <Atul Rai@AtulRai-PC>
2015-09-17 22:48:33 +0530

    comment

--- JSPLearning/src/jsp/AccessEmp.java
+++ JSPLearning/src/jsp/AccessEmp.java
@@ -3,9 +3,9 @@ package jsp;
 public class AccessEmp {
 	public static void main(String args[]) {
 
-		Employee e = new Employee();// object is created
+		Employee e = new Employee();  // object is created
 
-		e.setName("AtulRai");// setting value to the object
+		e.setName("AtulRai");  // setting value to the object
 
 		System.out.println(e.getName());
 
