daba11c34c2ccf99335a840fe8aaa36050efce35
Atul Rai <Atul Rai@AtulRai-PC>
2015-09-17 22:43:38 +0530

    java beans

---
+++ JSPLearning/src/jsp/AccessEmp.java
@@ -0,0 +1,13 @@
+package jsp;
+
+public class AccessEmp {
+	public static void main(String[] args) {
+
+		Employee e = new Employee(); // object is created
+
+		e.setName("AtulRai"); // setting value to the object
+
+		System.out.println(e.getName());
+	}
+
+}
