d848b8e4f8ed585d3d903b7b980b57bb00a17173
Atul Rai <Atul Rai@AtulRai-PC>
2015-09-18 09:03:18 +0530

    jsp plugin

---
+++ JSPLearning/src/jsp/MouseDrag.java
@@ -0,0 +1,25 @@
+package jsp;
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class MouseDrag extends Applet implements MouseMotionListener {
+
+	public void init() {
+		addMouseMotionListener(this);
+		setBackground(Color.red);
+	}
+
+	public void mouseDragged(MouseEvent me) {
+		Graphics g = getGraphics();
+		g.setColor(Color.black);
+		g.fillOval(me.getX(), me.getY(), 20, 20);
+	}
+
+	public void mouseMoved(MouseEvent me) {
+	}
+
+}
