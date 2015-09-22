b54a04d8290b46be4dddaeff30af6018293f8fe1
Atul Rai <Atul Rai@AtulRai-PC>
2015-09-22 21:11:31 +0530

    servlet

---
+++ ServletLearning/src/atulrai/FirstServlet.java
@@ -0,0 +1,43 @@
+package atulrai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class FirstServlet implements Servlet {
+
+	ServletConfig config = null;
+
+	public void init(ServletConfig config) {
+		this.config = config;
+		System.out.println("Servlet is initialized");
+	}
+
+	public void service(ServletRequest req, ServletResponse res)
+			throws IOException, ServletException {
+		res.setContentType("text/html");
+
+		PrintWriter out = res.getWriter();
+		out.print("<html><body>");
+		out.print("<h3>Hello Simple Servlet</h3>");
+		out.print("</body></html>");
+	}
+
+	public void destroy() {
+		System.out.println("Servlet is destroyed");
+	}
+
+	public ServletConfig getServletConfig() {
+		return config;
+	}
+
+	public String getServletInfo() {
+		return "copyright @Atul Rai";
+	}
+
+}
