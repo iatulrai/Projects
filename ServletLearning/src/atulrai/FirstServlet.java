b4c8fbdee30a8df1c9b20fda326296565574f10c
Atul Rai <Atul Rai@AtulRai-PC>
2015-09-23 01:10:31 +0530

    added and deleted

--- ServletLearning/src/atulrai/FirstServlet.java
+++ ServletLearning/src/atulrai/FirstServlet.java
@@ -8,28 +8,34 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
 public class FirstServlet implements Servlet {
-
 	ServletConfig config = null;
 
 	public void init(ServletConfig config) {
 		this.config = config;
-		System.out.println("Servlet is initialized");
+		System.out.println("servlet is initialized");
 	}
 
 	public void service(ServletRequest req, ServletResponse res)
 			throws IOException, ServletException {
+
 		res.setContentType("text/html");
 
 		PrintWriter out = res.getWriter();
 		out.print("<html><body>");
-		out.print("<h3>Hello Simple Servlet</h3>");
+		out.print("<b>hello simple servlet</b>");
 		out.print("</body></html>");
+
 	}
 
 	public void destroy() {
-		System.out.println("Servlet is destroyed");
+		System.out.println("servlet is destroyed");
 	}
 
 	public ServletConfig getServletConfig() {
