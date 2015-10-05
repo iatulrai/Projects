b9893f910b489f1371d804fb48473a9d83025c9a
Atul Rai <Atul Rai@AtulRai-PC>
2015-10-05 07:54:23 +0530

    filter

---
+++ Servlet/src/atulrai/SendingResponseFilter.java
@@ -0,0 +1,29 @@
+package atulrai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class SendingResponseFilter implements Filter {
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain chain) throws IOException, ServletException {
+
+		PrintWriter out = res.getWriter();
+
+		out.print("<br/>this site is underconstruction..");
+		out.close();
+
+	}
+
+	public void destroy() {
+	}
+}
