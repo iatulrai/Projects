5c9e85b64a73a510c52742704861d9a920b4511c
Atul Rai <Atul Rai@AtulRai-PC>
2015-10-06 20:22:38 +0530

    uploading file to the server in servlet

---
+++ Servlet/src/atulrai/UploadFileServlet.java
@@ -0,0 +1,40 @@
+// uploading file to the server in servlet
+
+package atulrai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.oreilly.servlet.MultipartRequest;
+
+/**
+ * Servlet implementation class UploadFileServlet
+ */
+@WebServlet("/UploadFileServlet")
+public class UploadFileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+		MultipartRequest m = new MultipartRequest(request,
+				"D:\\Work-Study\\Servlet\\Upload");
+
+		out.print("Successfully uploaded :)");
+	}
+
+}
