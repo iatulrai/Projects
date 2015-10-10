0e097840bbb44fad88362921decb735497d28fe2
Atul Rai <Atul Rai@AtulRai-PC>
2015-10-10 22:52:57 +0530

    update

--- ThePic/src/com/thepic/MainActivity.java
+++ ThePic/src/com/thepic/MainActivity.java
@@ -3,7 +3,6 @@ package com.thepic;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
-import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 	@Override
