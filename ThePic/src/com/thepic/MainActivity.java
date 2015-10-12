eb5efa220cdab5848974308e4086c8a6f05d2fe3
Atul Rai <Atul Rai@AtulRai-PC>
2015-10-12 10:15:10 +0530

    Android Screen Orientation

--- ThePic/src/com/thepic/MainActivity.java
+++ ThePic/src/com/thepic/MainActivity.java
@@ -2,17 +2,31 @@ package com.thepic;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+	EditText editText1;
+	Button button1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		requestWindowFeature(Window.FEATURE_NO_TITLE); // it will hide the
+		// requestWindowFeature(Window.FEATURE_NO_TITLE); // it will hide the
 		// tile of app
 
+		editText1 = (EditText) findViewById(R.id.editText1);
+		button1 = (Button) findViewById(R.id.button1);
+
 		setContentView(R.layout.activity_main);
+	}
+
+	public void onClick(View v) {
+		editText1.setText("O android");
 	}
 
 }
