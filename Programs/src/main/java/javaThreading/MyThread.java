1046fb89b2b40d96544ca625c913d5cbc89282c5
Atul Rai <Atul Rai@AtulRai-PC>
2016-01-17 10:21:37 +0530

    update

--- Programs/src/main/java/javaThreading/MyThread.java
+++ Programs/src/main/java/javaThreading/MyThread.java
@@ -1,25 +1,26 @@
 package javaThreading;
 
 public class MyThread extends Thread {
-	
+
 	public void run() {
-		
+
 		System.out.println("shutdown hook task completed");
 
 	}
-	
-	public static  class Shutdown{
-		public static void main(String[] args) throws Exception{
+
+	public static class Shutdown {
+		public static void main(String[] args) throws Exception {
 			Runtime r = Runtime.getRuntime();
 			r.addShutdownHook(new MyThread());
-			
+
 			System.out.println("Now sleeping.... press ctrl+c to exit");
-			try{
+			try {
 				Thread.sleep(2000);
-			} catch (InterruptedException ex){
+			} catch (InterruptedException ex) {
+
 				ex.printStackTrace();
 			}
-			
+
 		}
 	}
 
