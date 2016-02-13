51e162c09943e5e7175b0bf9853616f256001c36
Agile <Agile@192.168.1.35>
2016-02-13 12:16:54 +0530

    NIO from java doc- Agile

---
+++ JavaDoc/src/com/nio/CopyBytes.java
@@ -0,0 +1,43 @@
+package com.nio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyBytes {
+	
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream in = null;
+		FileOutputStream out = null;
+
+		try {
+
+			in = new FileInputStream("ByteStreamIn.txt");
+			out = new FileOutputStream("ByteStreamOut.txt");
+
+			int c;
+
+			while ((c = in.read()) != -1) {
+
+				out.write(c);
+
+			}
+			
+			System.out.println("File copied");
+
+		} finally {
+
+			if (in != null) {
+				in.close();
+			}
+
+			if (out != null) {
+				out.close();
+			}
+
+		}
+
+	}
+
+}
